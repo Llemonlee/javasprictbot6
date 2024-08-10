91bdc794b658ce811feebcb8c96257e69c0158f7
Llemonlee <30706lilee@gmail.com>
2024-08-10 22:05:10 +0800

    EP20.更新

--- src/commands/clear/index.js
+++ src/commands/clear/index.js
@@ -1,10 +1,24 @@
-import {SlashCommandBuilder} from 'discord.js'
-
+import { SlashCommandBuilder } from 'discord.js';
 
 export const command = new SlashCommandBuilder()
- .setName("hi")
- .setDescription("hi command")
+  .setName('clear')
+  .setDescription('清除頻道消息');
 
-export const action = async(ctx) =>{
- await ctx.reply('早安!')
-}
+export const action = async (ctx) => {
+  if (!ctx.member.permissions.has('MANAGE_MESSAGES')) {
+    await ctx.reply('你不能用啦!!');
+    return;
+  }
+
+  // 获取频道的消息
+  const fetchedMessages = await ctx.channel.messages.fetch({ limit: 100 });
+
+  // 删除所有获取的消息
+  try {
+    await Promise.all(fetchedMessages.map(message => message.delete()));
+    await ctx.reply('清除!!');
+  } catch (error) {
+    console.error('清除消息失败:', error);
+    await ctx.reply('清除消息时发生错误。');
+  }
+};
